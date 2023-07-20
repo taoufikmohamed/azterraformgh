be562855c17bee1cb2e5a304dff3cf68373e8b2f
Mohamed Taoufik <26463859+taoufikmohamed@users.noreply.github.com>
2023-07-21 01:19:03 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "azurerm_key_vault" "tfkv" {
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id #"ef80a7c8-e662-4d93-8648-c458911e6f5f"
+  tenant_id                   = data.azurerm_client_config.current.tenant_id 
   #soft_delete_retention_days  = 7
   #purge_protection_enabled    = false
 
